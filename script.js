96e52a13ecbf321a6d1d88374223a4ff8f3019f1
Talha Anwar <mtalhaanwar359@gmail.com>
2024-05-13 07:45:07 +0500

    final

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
